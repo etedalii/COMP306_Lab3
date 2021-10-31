bbcc9af0f67e9f92715a1b71e8ef781e7c9e5c28
Mohammad Etedali <etedali@gmail.com>
2021-10-31 16:34:42 -0400

    .

--- 301104393Lu_Mohammad_Lab3/ViewModels/Enum.cs
+++ 301104393Lu_Mohammad_Lab3/ViewModels/Enum.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace _301104393Lu_Mohammad_Lab3
+﻿namespace _301104393Lu_Mohammad_Lab3
 {
     public enum Genre
     {
         Action,
         Superhero,
-        Documentary,
         Drama,
         Comedy,
-        Short,
         Romance,
         History
     }
